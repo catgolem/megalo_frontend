063b0f294c951fd7150721d7eddb01b6df8d8ef1
MH3292 <106174422+MitsukuniH@users.noreply.github.com>
2022-12-17 23:52:43 +0000

    modify

--- src/AddEventButton.js
+++ src/AddEventButton.js
@@ -36,102 +36,41 @@ export function AddEventButton(){
         .catch(e=>console.log(e));
       }
 
-    // function AddEventMenu(){
-    //     const [stage, setStage] = useState(0);
-    //     const [options, setOptions] = useState([]);
-    //     const [flgs, setFlgs] = useState({addLocation: false, addUrl: false});
-    //     const {register, handleSubmit} = useForm({mode: "onSubmit"});
-    //     const onSubmit = data=> console.log(data);
-
-    //     const tagsItem = ["none", "meeting", "odekake"].map(t=><option value={t}>{t}</option>);
-        
-    //     const OptionStage = ()=>{
-    //         switch(stage){
-    //         case 0:
-    //             return <button type="button" className="btn" onClick={()=>setStage(1)}>+Option</button>
-    //         case 1:                
-    //             return (
-    //                 <select onChange={(e)=>{
-    //                     console.log(e.target.value);
-    //                     if(e.target.value === "location"){
-    //                         setOptions(options.concat([<p>location</p>,<input type="text" {...register("lacation", {required: true})}/>]));
-    //                         setFlgs({addLocation: true, addUrl: flgs.addUrl});
-    //                     }
-    //                     else if(e.target.value === "url"){
-    //                         setOptions(options.concat([<p>url</p>, <input type="url" {...register("url", {required: true})}/>]));
-    //                         setFlgs({addLocation: flgs.addLocation, addUrl: true});
-    //                     }
-    //                     setStage(0)
-    //                 }} >
-    //                     <option value="none">---</option>
-    //                     {flgs.addLocation?<></>: <option value="location">location</option>}
-    //                     {flgs.addUrl?<></>:<option value="url">url</option>}
-    //                 </select>
-    //             )
-    //         }
-    //     }
-    //     const optionsItem = options.map((o)=>o);
-    //     return (
-    //         <div id="overlay" style={overlay}>
-    //             <div id="modalContent" className="bg-[#ddddbb] rounded-md p-10">
-    //                 <div className="grid grid-cols-[1fr,_60px] grid-rows-[30px]">
-    //                     <p className="text-center">AddEvent</p>
-    //                     <button onClick={()=>setShowAddMenu(false)} className='btn px-1'>Cansel</button>
-    //                 </div>
-    //                 <form onSubmit={handleSubmit(onSubmit)} className="grid grid-cols-[80px,_220px]">
-    //                     <p>Title</p>
-    //                     <input type="text" {...register("title", {required: true})} />
-    //                     <p>Start</p>
-    //                     <input type="time" {...register("start", {required: true})}/>
-    //                     <p>end</p>
-    //                     <input type="time" {...register("end", {required: true})}/>
-    //                     <p>tag</p>
-    //                     <select {...register("tag", {required: false})}>{tagsItem}</select>
-    //                     <p>content</p>
-    //                     <textarea {...register("content", {required: false})}/>
-    //                     {optionsItem}
-    //                     <OptionStage/>
-    //                     <button type="submit" className="btn" onClick={()=>setShowAddMenu(false)}>add</button>
-    //                 </form>
-    //             </div>
-    //         </div>
-    //     )
-    // }
-    function AddEventMenuTest(){
-        // const [stage, setStage] = useState(0);
-        // const [options, setOptions] = useState([]);
-        // const [flgs, setFlgs] = useState({addLocation: false, addUrl: false});
+    function AddEventMenu(){
+        const [stage, setStage] = useState(0);
+        const [options, setOptions] = useState([]);
+        const [flgs, setFlgs] = useState({addLocation: false, addUrl: false});
         const {register, handleSubmit} = useForm({mode: "onSubmit"});
         const onSubmit = data=> console.log(data);
 
         const tagsItem = ["none", "meeting", "odekake"].map(t=><option value={t}>{t}</option>);
         
-        // const OptionStage = ()=>{
-        //     switch(stage){
-        //     case 0:
-        //         return <button type="button" className="btn" onClick={()=>setStage(1)}>+Option</button>
-        //     case 1:                
-        //         return (
-        //             <select onChange={(e)=>{
-        //                 console.log(e.target.value);
-        //                 if(e.target.value === "location"){
-        //                     setOptions(options.concat([<p>location</p>,<input type="text" {...register("lacation", {required: true})}/>]));
-        //                     setFlgs({addLocation: true, addUrl: flgs.addUrl});
-        //                 }
-        //                 else if(e.target.value === "url"){
-        //                     setOptions(options.concat([<p>url</p>, <input type="url" {...register("url", {required: true})}/>]));
-        //                     setFlgs({addLocation: flgs.addLocation, addUrl: true});
-        //                 }
-        //                 setStage(0)
-        //             }} >
-        //                 <option value="none">---</option>
-        //                 {flgs.addLocation?<></>: <option value="location">location</option>}
-        //                 {flgs.addUrl?<></>:<option value="url">url</option>}
-        //             </select>
-        //         )
-        //     }
-        // }
-        //const optionsItem = options.map((o)=>o);
+        const OptionStage = ()=>{
+            switch(stage){
+            case 0:
+                return <button type="button" className="btn" onClick={()=>setStage(1)}>+Option</button>
+            case 1:                
+                return (
+                    <select onChange={(e)=>{
+                        console.log(e.target.value);
+                        if(e.target.value === "location"){
+                            setOptions(options.concat([<p>location</p>,<input type="text" {...register("lacation", {required: true})}/>]));
+                            setFlgs({addLocation: true, addUrl: flgs.addUrl});
+                        }
+                        else if(e.target.value === "url"){
+                            setOptions(options.concat([<p>url</p>, <input type="url" {...register("url", {required: true})}/>]));
+                            setFlgs({addLocation: flgs.addLocation, addUrl: true});
+                        }
+                        setStage(0)
+                    }} >
+                        <option value="none">---</option>
+                        {flgs.addLocation?<></>: <option value="location">location</option>}
+                        {flgs.addUrl?<></>:<option value="url">url</option>}
+                    </select>
+                )
+            }
+        }
+        const optionsItem = options.map((o)=>o);
         return (
             <div id="overlay" style={overlay}>
                 <div id="modalContent" className="bg-[#ddddbb] rounded-md p-10">
@@ -150,20 +89,21 @@ export function AddEventButton(){
                         <select {...register("tag", {required: false})}>{tagsItem}</select>
                         <p>content</p>
                         <textarea {...register("content", {required: false})}/>
-                        {/* {optionsItem} */}
-                        {/* <OptionStage/> */}
+                        {optionsItem}
+                        <OptionStage/>
                         <button type="submit" className="btn" onClick={()=>setShowAddMenu(false)}>add</button>
                     </form>
                 </div>
             </div>
         )
     }
+    
 
     return(
         <>
             <button onClick={()=>setShowAddMenu(true)} className="btn p-10">+</button>
             {showAddMenu && (
-                <AddEventMenuTest/>
+                <AddEventMenu/>
             )
             }
         </>
--- src/App.js
+++ src/App.js
@@ -23,50 +23,10 @@ function App() {
     // postTask()
     getTask()
   });
-  //debug
-  function AddEventMenu(){
-        const [stage, setStage] = useState(0);
-        const [options, setOptions] = useState([]);
-        const [flgs, setFlgs] = useState({addLocation: false, addUrl: false});
-        const {register, handleSubmit} = useForm({mode: "onSubmit"});
-        const onSubmit = data=> console.log(data);
-
-        const tagsItem = ["none", "meeting", "odekake"].map(t=><option value={t}>{t}</option>);
-        
-        
-        const optionsItem = options.map((o)=>o);
-        return (
-            <div id="overlay">
-                <div id="modalContent" className="bg-[#ddddbb] rounded-md p-10">
-                    <div className="grid grid-cols-[1fr,_60px] grid-rows-[30px]">
-                        <p className="text-center">AddEvent</p>
-                        <button className='btn px-1'>Cansel</button>
-                    </div>
-                    <form onSubmit={handleSubmit(onSubmit)} className="grid grid-cols-[80px,_220px]">
-                        <p>Title</p>
-                        <input type="text" {...register("title", {required: true})} />
-                        <p>Start</p>
-                        <input type="time" {...register("start", {required: true})}/>
-                        <p>end</p>
-                        <input type="time" {...register("end", {required: true})}/>
-                        <p>tag</p>
-                        <select {...register("tag", {required: false})}>{tagsItem}</select>
-                        <p>content</p>
-                        <textarea {...register("content", {required: false})}/>
-                        {optionsItem}
-                        <button type="submit" className="btn">add</button>
-                    </form>
-                </div>
-            </div>
-        )
-    }
-    //debug
   return (
     <div className='grid grid-rows-[100px,_30px,_1000px]'>
       <RecentSchedule data={data}/>
       <div className='fixed bottom-10 right-10 p-2'><AddEventButton/></div>
-      <button className="btn p-10">+</button>
-            <AddEventMenu/>
     </div>
   );
 }
