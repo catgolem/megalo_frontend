a559697557b434992ea646f191238a30496c272d
Aopaka1231 <114895847+Aopaka1231@users.noreply.github.com>
2022-12-17 09:35:18 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import {Tab, Tabs, TabList, TabPanel} from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import Todaysche from './Todayschedule';
+import Monthsche from './monthschedelu';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Tabs className="Tab">
+      <TabList>
+          <Tab>1日のスケジュール</Tab>
+          <Tab>月のスケジュール</Tab>
+          <Tab>設定</Tab>
+      </TabList>
+      <TabPanel>
+        <Todaysche/>
+      </TabPanel>
+      <TabPanel>
+          <h2>
+          </h2>
+      </TabPanel>
+      <TabPanel>
+          <h2>
+              タグの追加とか
+          </h2>
+      </TabPanel>
+  </Tabs>
   );
 }
 
