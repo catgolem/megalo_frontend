5374729dcafb5cd4e80a3b9d3a058e2df21e93de
MH3292 <106174422+MitsukuniH@users.noreply.github.com>
2022-12-17 23:41:59 +0000

    バグ確認

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import { RecentSchedule } from './RecentSchedule';
 import {useLayoutEffect, useState } from "react";
 import { AddEventButton } from './AddEventButton';
-
+import axios from 'axios';
+import { useForm } from 'react-hook-form';
 
 function App() {
   const [data ,setData] = useState([
@@ -12,18 +13,60 @@ function App() {
     { title: "kadai", content: "", date:20221219, start:1500}
   ]);
 
-  useLayoutEffect(() => {
-    var respose;
-    //APIをたたいて変数に代入する処理を書くAPIからのデータがrespose.dataに入る。
+  function getTask(){
+    axios.get("https://func-schedule.azurewebsites.net/api/TaskGet?code=I-A05Xoz0TAhmmrO6liS3S3eqXy0QHj7XihVcwVdrX4hAzFuMH5UbQ==&userId=123456789")
+    .then(response=>setData(response.data))
+    .catch(e=>console.log(e));
+  }
 
-    //setDataでdataにresponse.dataを代入する。
+  useLayoutEffect(() => {
+    // postTask()
+    getTask()
   });
-  
+  //debug
+  function AddEventMenu(){
+        const [stage, setStage] = useState(0);
+        const [options, setOptions] = useState([]);
+        const [flgs, setFlgs] = useState({addLocation: false, addUrl: false});
+        const {register, handleSubmit} = useForm({mode: "onSubmit"});
+        const onSubmit = data=> console.log(data);
+
+        const tagsItem = ["none", "meeting", "odekake"].map(t=><option value={t}>{t}</option>);
+        
+        
+        const optionsItem = options.map((o)=>o);
+        return (
+            <div id="overlay">
+                <div id="modalContent" className="bg-[#ddddbb] rounded-md p-10">
+                    <div className="grid grid-cols-[1fr,_60px] grid-rows-[30px]">
+                        <p className="text-center">AddEvent</p>
+                        <button className='btn px-1'>Cansel</button>
+                    </div>
+                    <form onSubmit={handleSubmit(onSubmit)} className="grid grid-cols-[80px,_220px]">
+                        <p>Title</p>
+                        <input type="text" {...register("title", {required: true})} />
+                        <p>Start</p>
+                        <input type="time" {...register("start", {required: true})}/>
+                        <p>end</p>
+                        <input type="time" {...register("end", {required: true})}/>
+                        <p>tag</p>
+                        <select {...register("tag", {required: false})}>{tagsItem}</select>
+                        <p>content</p>
+                        <textarea {...register("content", {required: false})}/>
+                        {optionsItem}
+                        <button type="submit" className="btn">add</button>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+    //debug
   return (
-    <div className='grid grid-rows-[100px,_30px]'>
+    <div className='grid grid-rows-[100px,_30px,_1000px]'>
       <RecentSchedule data={data}/>
       <div className='fixed bottom-10 right-10 p-2'><AddEventButton/></div>
-      
+      <button className="btn p-10">+</button>
+            <AddEventMenu/>
     </div>
   );
 }
