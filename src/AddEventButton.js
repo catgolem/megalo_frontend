8d9b00d909ec49a8a75f9ac859452847eef0feeb
MH3292 <106174422+MitsukuniH@users.noreply.github.com>
2022-12-18 03:00:51 +0000

    避難

--- src/AddEventButton.js
+++ src/AddEventButton.js
@@ -2,8 +2,6 @@ import userEvent from "@testing-library/user-event";
 import { useRef, useState } from "react"
 import { useForm } from "react-hook-form";
 
-
-
 export function AddEventButton(){    
     const overlay = {
         position: "fixed",
@@ -17,7 +15,6 @@ export function AddEventButton(){
         alignItems: "center",
         justifyContent: "center",
     };
-    const [showAddMenu, setShowAddMenu] = useState(false);
 
     function postTask(props){
         axios
@@ -35,16 +32,30 @@ export function AddEventButton(){
         .then(response=>console.log(response))
         .catch(e=>console.log(e));
       }
+    const [showMenu, setShowMenu] = useState(false);
 
-    function AddEventMenu(){
+    const ToggleShowMenu = (bool)=>{
+        setShowMenu(bool);
+    }
+
+
+    const AddEventMenu=()=>{
         const [stage, setStage] = useState(0);
         const [options, setOptions] = useState([]);
         const [flgs, setFlgs] = useState({addLocation: false, addUrl: false});
-        const {register, handleSubmit} = useForm({mode: "onSubmit"});
-        const onSubmit = data=> console.log(data);
-
+        //Form
+        const [title, setTitle] = useState();
+        const today = new Date();
+        const [date, setDate] = useState();
+        const [start, setStart] = useState();
+        const [end, setEnd] = useState();
+        const [tag, setTag] = useState();
+        const [content, setContent] = useState();
+        const [location, setLocation] = useState();
+        const [url, setUrl] = useState();
+        //Form
         const tagsItem = ["none", "meeting", "odekake"].map(t=><option value={t}>{t}</option>);
-        
+
         const OptionStage = ()=>{
             switch(stage){
             case 0:
@@ -54,11 +65,17 @@ export function AddEventButton(){
                     <select onChange={(e)=>{
                         console.log(e.target.value);
                         if(e.target.value === "location"){
-                            setOptions(options.concat([<p>location</p>,<input type="text" {...register("lacation", {required: true})}/>]));
+                            setOptions(options.concat([
+                                <p>location</p>,
+                                <input type="text" value={location} onChange={e=>setLocation(e.target.value)}/>
+                            ]));
                             setFlgs({addLocation: true, addUrl: flgs.addUrl});
                         }
                         else if(e.target.value === "url"){
-                            setOptions(options.concat([<p>url</p>, <input type="url" {...register("url", {required: true})}/>]));
+                            setOptions(options.concat([
+                                <p>url</p>, 
+                                <input type="url" value={url} onChange={e=>setUrl(e.target.value)}/>
+                            ]));
                             setFlgs({addLocation: flgs.addLocation, addUrl: true});
                         }
                         setStage(0)
@@ -70,42 +87,65 @@ export function AddEventButton(){
                 )
             }
         }
+        const handleSubmit = ()=>{
+            const data = {
+                title: title,
+                date: date,
+                start: start,
+                end: end,
+                content: content,
+                tag: tag,
+                location: location,
+                url: url
+            }
+            console.log(data);
+        }
         const optionsItem = options.map((o)=>o);
+
         return (
             <div id="overlay" style={overlay}>
                 <div id="modalContent" className="bg-[#ddddbb] rounded-md p-10">
                     <div className="grid grid-cols-[1fr,_60px] grid-rows-[30px]">
                         <p className="text-center">AddEvent</p>
-                        <button onClick={()=>setShowAddMenu(false)} className='btn px-1'>Cansel</button>
+                        <button onClick={()=>ToggleShowMenu(false)} className='btn px-1'>Cansel</button>
                     </div>
-                    <form onSubmit={handleSubmit(onSubmit)} className="grid grid-cols-[80px,_220px]">
+                    <div className="grid grid-cols-[80px,_220px]">
                         <p>Title</p>
-                        <input type="text" {...register("title", {required: true})} />
+                        <input type="text" value={title} onChange={e=>setTitle(e.target.value)} />
+                        <p>Date</p>
+                        <input type="date" value={date} onChange={e=>setDate(e.target.value)} />
                         <p>Start</p>
-                        <input type="time" {...register("start", {required: true})}/>
+                        <input type="time" value={start} onChange={e=>setStart(e.target.value)}/>
                         <p>end</p>
-                        <input type="time" {...register("end", {required: true})}/>
+                        <input type="time" value={end} onChange={e=>setEnd(e.target.value)}/>
                         <p>tag</p>
-                        <select {...register("tag", {required: false})}>{tagsItem}</select>
+                        <select  value={tag} onChange={e=>setTag(e.target.value)}>{tagsItem}</select>
                         <p>content</p>
-                        <textarea {...register("content", {required: false})}/>
+                        <textarea  value={content} onChange={e=>setContent(e.target.value)}/>
                         {optionsItem}
                         <OptionStage/>
-                        <button type="submit" className="btn" onClick={()=>setShowAddMenu(false)}>add</button>
-                    </form>
+                        <button className="btn" onClick={handleSubmit}>add</button>
+                    </div>
                 </div>
             </div>
         )
     }
-    
 
+    const Test =()=>{
+        const {register, handleSubmit} = useForm();
+        return(
+            <form onSubmit={handleSubmit(d=>console.log(d))}>
+                <input {...register("tad")}/>
+                <button className="btn">btn</button>
+            </form>
+        )
+    }
+    
+    //const [showAddMenu, setShowAddMenu] = useState(false);
     return(
         <>
-            <button onClick={()=>setShowAddMenu(true)} className="btn p-10">+</button>
-            {showAddMenu && (
-                <AddEventMenu/>
-            )
-            }
+            <button onClick={()=>ToggleShowMenu(true)} className="btn p-10">+</button>
+            {showMenu && <AddEventMenu/>}
         </>
     )
 }
